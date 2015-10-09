989e34e80d7d73d8dba5dec520aea64fc8ce3b8c
THCaptain <korniax@gmail.com>
2015-10-09 08:53:43 +0200

    update commit

--- Others/FtoC.py
+++ Others/FtoC.py
@@ -1,10 +1,10 @@
-import math
+from __future__ import division
 
 def Celtofah(cel):
-    Fah = (cel*1.8) + 32
+    Fah = (cel*1.8) + 32.0
     return Fah
 def Fahtocel(Fah):
-    Cel = (Fah - 32) / 1.8
+    Cel = (Fah - 32.0) / 1.8
     return Cel
 
 x = 0
@@ -12,16 +12,16 @@ y = 0
 a = 0
 
 print "Enter the temperature to convert:"
-x = input('>')
+x = float(input('>'))
 print "Enter 1 for Celsius to Fahrenheit, 2 for Fahrenheit to Celsius"
-y = input('>')
+y = float(input('>'))
 
 if y == 1:
     a = Celtofah(x)
-    print "\n>>Celsius to Fahrenheit. \n %d C = %d F" % (x, a)
+    print "\n>>Celsius to Fahrenheit. \n %f C = %f F" % (x, a)
 elif y == 2:
     a = Fahtocel(x)
-    print "\n>>Fahrenheit to Celsius. \n %d F = %d C" % (x, a)
+    print "\n>>Fahrenheit to Celsius. \n %f F = %f C" % (x, a)
 elif y != 1 or 2:
     print 'Invalid input, you doofus.'
     exit(0)
