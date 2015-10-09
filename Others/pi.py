2fead5abf34b3782353e8b259fc13cc4325f5c75
THCaptain <korniax@gmail.com>
2015-10-09 12:34:21 +0200

    pi2.0

--- Others/pi.py
+++ Others/pi.py
@@ -1,5 +1,25 @@
-import math
-from decimal import *
+from __future__ import with_statement #code from stackoverflow answers
+import decimal                          # https://stackoverflow.com/questions/347734/gauss-legendre-algorithm-in-python
 
-x = Decimal(math.pi)
-print "%.*f" % (256, x)
+
+def pi_gauss_legendre():
+    D = decimal.Decimal
+    with decimal.localcontext() as ctx:
+        ctx.prec += 2                
+        a, b, t, p = 1, 1/D(2).sqrt(), 1/D(4), 1                #
+        pi = None                                               #
+        while 1:                                                #formula from Gauss Legendre algorithm :
+            an = (a + b) / 2                                    #https://en.wikipedia.org/wiki/Gauss%E2%80%93Legendre_algorithm
+            b = (a * b).sqrt()                                  #
+            t -= p * (a - an) * (a - an)                        #
+            a, p = an, 2*p                                      #
+            piold = pi                                          #
+            pi = (a + b) * (a + b) / (4 * t)                    #
+            if pi == piold:                                     # equal within given precision
+                break                                           #
+    return +pi
+
+
+decimal.getcontext().prec = 2500
+print pi_gauss_legendre()
+
